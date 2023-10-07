da88ca08f70f964423ea251680383fb2b02007a0
Andrey <88864809+AndreyPinchukDeveloper@users.noreply.github.com>
2023-10-07 20:06:56 +0700

    dev Update #1
    - added program.cs settings.

--- WebAPI/Program.cs
+++ WebAPI/Program.cs
@@ -1,9 +1,18 @@
-var builder = WebApplication.CreateBuilder(args);
+using Microsoft.EntityFrameworkCore;
 
-// Add services to the container.
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+var builder = WebApplication.CreateBuilder(args);
+var services = builder.Services;
+
+ConfigurationManager configuration = builder.Configuration;
+var option = configuration.GetConnectionString("DefaultConnection");
+builder.Services.AddDbContext<ApplicationDbContext>( options =>
+    options.UseSqlServer(configuration.GetConnectionString(option)));
+
+services.AddEndpointsApiExplorer();
+services.AddMyOwnAutoMapper();
+services.AddSwaggerGen();
+services.AddMyOwnScopedServices();
+services.AddMvc();
 
 var app = builder.Build();
 
@@ -15,30 +24,10 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
+app.UseAuthentication();
+app.UseAuthorization();
 
-var summaries = new[]
-{
-    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-};
+app.MapControllers();
 
-app.MapGet("/weatherforecast", () =>
-{
-    var forecast = Enumerable.Range(1, 5).Select(index =>
-        new WeatherForecast
-        (
-            DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-            Random.Shared.Next(-20, 55),
-            summaries[Random.Shared.Next(summaries.Length)]
-        ))
-        .ToArray();
-    return forecast;
-})
-.WithName("GetWeatherForecast")
-.WithOpenApi();
 
 app.Run();
-
-internal record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
-{
-    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
-}
